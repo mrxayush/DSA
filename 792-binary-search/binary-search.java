a6051e7f12e7feb17fd08927eddeaad822c370c7
mrxayush <ayushlnctian1@gmail.com>
2026-04-27 18:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (32.39%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int n = nums.length;
+        int high = n-1;
+
+         while(low<=high)
+         {
+            int guess = (low+high)/2;
+            if(nums[guess]==target)
+            {
+                return guess;
+            }
+            else if(nums[guess]<target)
+            {
+                low = guess +1;
+            }
+            else{
+                high = guess -1;
+            }
+         }
+        return -1;
+    }
+}
